b6b501f04786b1548df69f7ea76fecc4d4aa97c7
Matheus Miranda <matheus.sm86@gmail.com>
2022-04-12 13:41:39 -0300

    [FIX] - Update parser to show data if checking on a day after the stock market is closed

--- app/src/main/java/com/msmlabs/stockmarket/data/csv/IntraDayInfoParser.kt
+++ app/src/main/java/com/msmlabs/stockmarket/data/csv/IntraDayInfoParser.kt
@@ -8,7 +8,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.InputStream
 import java.io.InputStreamReader
-import java.time.LocalDateTime
+import java.time.LocalDate
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -27,7 +27,7 @@ class IntraDayInfoParser @Inject constructor() : CsvParser<IntraDayInfo> {
                     dto.toIntraDayInfo()
                 }
                 .filter {
-                    it.date.dayOfMonth == LocalDateTime.now().minusDays(1).dayOfMonth
+                    it.date.dayOfMonth == LocalDate.now().minusDays(4).dayOfMonth
                 }
                 .sortedBy {
                     it.date.hour
