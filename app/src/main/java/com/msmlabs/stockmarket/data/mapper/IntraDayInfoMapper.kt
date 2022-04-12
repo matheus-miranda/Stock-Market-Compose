12336f98f3402659d61908e329130866429a2727
Matheus Miranda <matheus.sm86@gmail.com>
2022-04-11 23:21:43 -0300

    [FEAT] - Add coreLibraryDesugaring for backwards compatibility

--- app/src/main/java/com/msmlabs/stockmarket/data/mapper/IntraDayInfoMapper.kt
+++ app/src/main/java/com/msmlabs/stockmarket/data/mapper/IntraDayInfoMapper.kt
@@ -1,14 +1,11 @@
 package com.msmlabs.stockmarket.data.mapper
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import com.msmlabs.stockmarket.data.remote.dto.IntraDayInfoDto
 import com.msmlabs.stockmarket.domain.model.IntraDayInfo
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
 
-@RequiresApi(Build.VERSION_CODES.O)
 fun IntraDayInfoDto.toIntraDayInfo(): IntraDayInfo {
     val pattern = "yyyy-MM-dd HH:mm:ss"
     val formatter = DateTimeFormatter.ofPattern(pattern, Locale.getDefault())
