fb9f3345398be9cc8ea600ca0fd963a30effcb2f
Matheus Miranda <matheus.sm86@gmail.com>
2022-04-11 23:04:39 -0300

    [FEAT] - Create CompanyInfo model, dto and mapper

--- app/src/main/java/com/msmlabs/stockmarket/data/mapper/CompanyMapper.kt
+++ app/src/main/java/com/msmlabs/stockmarket/data/mapper/CompanyMapper.kt
@@ -1,6 +1,8 @@
 package com.msmlabs.stockmarket.data.mapper
 
 import com.msmlabs.stockmarket.data.local.CompanyListingEntity
+import com.msmlabs.stockmarket.data.remote.dto.CompanyInfoDto
+import com.msmlabs.stockmarket.domain.model.CompanyInfo
 import com.msmlabs.stockmarket.domain.model.CompanyListing
 
 fun CompanyListingEntity.toCompanyListing() = CompanyListing(
@@ -13,4 +15,12 @@ fun CompanyListing.toCompanyListingEntity() = CompanyListingEntity(
     symbol = this.symbol,
     name = this.name,
     exchange = this.exchange
+)
+
+fun CompanyInfoDto.toCompanyInfo() = CompanyInfo(
+    symbol = this.symbol ?: "",
+    description = this.description ?: "",
+    name = this.name ?: "",
+    country = this.country ?: "",
+    industry = this.industry ?: ""
 )
