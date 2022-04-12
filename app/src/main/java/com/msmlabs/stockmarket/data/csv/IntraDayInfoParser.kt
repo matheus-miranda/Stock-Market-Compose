be131b5d2813e4ef315034875bc1063444027659
Matheus Miranda <matheus.sm86@gmail.com>
2022-04-11 23:34:51 -0300

    [FEAT] - Create IntraDayInfoParser

---
+++ app/src/main/java/com/msmlabs/stockmarket/data/csv/IntraDayInfoParser.kt
@@ -0,0 +1,40 @@
+package com.msmlabs.stockmarket.data.csv
+
+import com.msmlabs.stockmarket.data.mapper.toIntraDayInfo
+import com.msmlabs.stockmarket.data.remote.dto.IntraDayInfoDto
+import com.msmlabs.stockmarket.domain.model.IntraDayInfo
+import com.opencsv.CSVReader
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.time.LocalDateTime
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class IntraDayInfoParser @Inject constructor() : CsvParser<IntraDayInfo> {
+
+    override suspend fun parse(stream: InputStream): List<IntraDayInfo> {
+        val csvReader = CSVReader(InputStreamReader(stream))
+        return withContext(Dispatchers.IO) {
+            csvReader.readAll()
+                .drop(1)
+                .mapNotNull { line ->
+                    val timestamp = line.getOrNull(0) ?: return@mapNotNull null
+                    val close = line.getOrNull(4) ?: return@mapNotNull null
+                    val dto = IntraDayInfoDto(timestamp, close.toDouble())
+                    dto.toIntraDayInfo()
+                }
+                .filter {
+                    it.date.dayOfMonth == LocalDateTime.now().minusDays(1).dayOfMonth
+                }
+                .sortedBy {
+                    it.date.hour
+                }
+                .also {
+                    csvReader.close()
+                }
+        }
+    }
+}
