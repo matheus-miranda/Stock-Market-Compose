6dc26badc5608271a1e8a4f485bcf9e30b1be730
Matheus Miranda <matheus.sm86@gmail.com>
2022-04-11 21:40:46 -0300

    [FIX] - Minor corrections on Database & Repository

--- app/src/main/java/com/msmlabs/stockmarket/data/local/StockDatabase.kt
+++ app/src/main/java/com/msmlabs/stockmarket/data/local/StockDatabase.kt
@@ -1,12 +1,13 @@
 package com.msmlabs.stockmarket.data.local
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(
     entities = [CompanyListingEntity::class],
     version = 1,
     exportSchema = false
 )
-abstract class StockDatabase {
+abstract class StockDatabase : RoomDatabase() {
     abstract val dao: StockDao
 }
--- app/src/main/java/com/msmlabs/stockmarket/data/repository/StockRepositoryImpl.kt
+++ app/src/main/java/com/msmlabs/stockmarket/data/repository/StockRepositoryImpl.kt
@@ -18,7 +18,9 @@ import javax.inject.Singleton
 
 @Singleton
 class StockRepositoryImpl @Inject constructor(
-    val api: StockApi, db: StockDatabase, val companyListingParser: CsvParser<CompanyListing>
+    private val api: StockApi,
+    db: StockDatabase,
+    private val companyListingParser: CsvParser<CompanyListing>
 ) : StockRepository {
 
     private val dao = db.dao
