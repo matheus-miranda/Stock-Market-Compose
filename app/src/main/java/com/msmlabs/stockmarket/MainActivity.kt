6e98101bf5f72d84e4601c7d1fd09648de476b53
Matheus Miranda <matheus.sm86@gmail.com>
2022-04-11 22:38:11 -0300

    [FEAT] - Show destination navigation and show companies when app opens

--- app/src/main/java/com/msmlabs/stockmarket/MainActivity.kt
+++ app/src/main/java/com/msmlabs/stockmarket/MainActivity.kt
@@ -6,38 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.msmlabs.stockmarket.presentation.listings.NavGraphs
 import com.msmlabs.stockmarket.ui.theme.StockMarketTheme
+import com.ramcosta.composedestinations.DestinationsNavHost
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             StockMarketTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    DestinationsNavHost(navGraph = NavGraphs.root)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    StockMarketTheme {
-        Greeting("Android")
     }
 }
